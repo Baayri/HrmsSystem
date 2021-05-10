eecf067ffc651f65e32d47f1aafe5c8114a33a2e
Muhammed Said Bayri <78660206+Baayri@users.noreply.github.com>
2021-05-11 02:00:00 +0300

    Update HrmsApplication.java

--- hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -3,7 +3,6 @@ package kodlamaio.hrms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import kodlamaio.hrms.entities.concretes.Job;
 
 @SpringBootApplication
 public class HrmsApplication {
